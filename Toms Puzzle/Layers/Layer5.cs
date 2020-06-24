41591cf8cf4d84c46106010757fd294c791c5596
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-06-24 20:28:25 +0100

    Layer 5 done!
    Layer 5 has been intentionally crippled, changing on line in the layer 5 decoder will reveal THE CORE

--- Toms Puzzle/Layers/Layer5.cs
+++ Toms Puzzle/Layers/Layer5.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿/*
+    To protect the innocent Layer 5 is closed for today, but if you come back and perform a simple edit to one line of code in this file then functionality will be restored
+*/
+
+using System;
 using System.IO;
 using Toms_Puzzle.Decoders;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
 using static Toms_Puzzle.Utilities.ByteConverter;
 
 namespace Toms_Puzzle.Layers
@@ -9,10 +17,12 @@ namespace Toms_Puzzle.Layers
     {
         // Properties
         public static byte[] KeyEncryptingKey { get; private set; }
-        public static byte[] InitializationVector { get; private set; }
-        public static byte[] EncryptionKey { get; private set; }
-        public static byte[] EncryptionInitializationVector { get; private set; }
+        public static byte[] KeyInitializationVector { get; private set; }
+        public static byte[] WrappedKey { get; private set; }
+        public static byte[] PayloadEncryptionKey { get; private set; } // The unwrapped key
+        public static byte[] PayloadInitializationVector { get; private set; }
 
+        // Decode the layer
         public static string DecodeLayer5(string payload, IDecoder decoder)
         {
             // Decode
@@ -27,10 +37,44 @@ namespace Toms_Puzzle.Layers
             // Get the encrypted payload
             byte[] encryptedPayload = GetEncryptedPayload(stream);
 
-            // Convert to string
-            string result = System.Text.Encoding.Default.GetString(encryptedPayload);
+            // Unwrap the wrapped key
+            byte[] decryptedPayloadBytes = Decrypt(encryptedPayload);
 
-            return result;
+            // Decode the payload
+            string decryptedPayload = System.Text.Encoding.Default.GetString(decryptedPayloadBytes, 0, decryptedPayloadBytes.Length - 5); // Ignore last 5 bytes, they're junk
+
+            return decryptedPayload;
+        }
+
+        // Unwraps a wrapped key using a key encrypting key (KEK) and Initialization Vector (IV)
+        private static byte[] UnwrapKey()
+        {
+            // Create Bouncy Castle AES Wrap Engine
+            IWrapper wrapper = new AesWrapEngine();
+
+            // Initialize engine with the KEK and IV
+            wrapper.Init(false, new ParametersWithIV(new KeyParameter(KeyEncryptingKey), KeyInitializationVector));
+            
+            // Unwrap the wrapped key
+            byte[] unwrapped = wrapper.Unwrap(WrappedKey, 0, WrappedKey.Length);
+
+            return unwrapped;
+        }
+
+        // Decrypt the encrypted payload
+        private static byte[] Decrypt(byte[] encryptedData)
+        {
+            // Unwrap the KEK
+            PayloadEncryptionKey = UnwrapKey();
+
+            // Setup the decryption cipher
+            IBufferedCipher cipher = CipherUtilities.GetCipher("AES/ECB/NoPadding");
+            cipher.Init(false, new ParametersWithIV(new KeyParameter(PayloadEncryptionKey), PayloadInitializationVector));
+
+            // Decrypt
+            byte[] decrypted = cipher.ProcessBytes(encryptedData);
+
+            return decrypted;
         }
 
         // Gets the AES keys and initialization vectors
@@ -42,16 +86,16 @@ namespace Toms_Puzzle.Layers
 
             // Get 64-bit Initialization Vector (8 bytes)
             int ivSize = 8;
-            InitializationVector = GetBytes(stream, ivSize);
+            KeyInitializationVector = GetBytes(stream, ivSize);
 
             // Get the wrapped (encrypted) key (40 bytes)
             // When unwrapped this will become the 256-bit Encryption Key
             int ekSize = 40;
-            EncryptionKey = GetBytes(stream, ekSize);
+            WrappedKey = GetBytes(stream, ekSize);
 
             // Get the 128-bit Initialization Vector for the encrypted payload (16 bytes)
             int eivSize = 16;
-            EncryptionInitializationVector = GetBytes(stream, eivSize);
+            PayloadInitializationVector = GetBytes(stream, eivSize);
         }
 
         // Get the encrypted payload from memory stream
